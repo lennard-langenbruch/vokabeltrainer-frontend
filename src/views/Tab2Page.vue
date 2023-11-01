e6df0d1402a13f43994ae355c11d7ddd35d0e0d6
Lennard Langenbruch <lennard.langenbruch@gmail.com>
2023-11-01 17:58:38 +0100

    modified tabs from template

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -8,7 +8,7 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
+          <ion-title size="large">Learn</ion-title>
         </ion-toolbar>
       </ion-header>
 
